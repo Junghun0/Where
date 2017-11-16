849195b463930bd5959f6242e3dca1daf453538d
lenovo <hjun4512@naver.com>
2017-11-16 20:30:45 +0900

    Google Map Push

--- app/src/main/java/com/example/parkjunghun/where/where/Fragment/MapFragment.java
+++ app/src/main/java/com/example/parkjunghun/where/where/Fragment/MapFragment.java
@@ -2,15 +2,15 @@ package com.example.parkjunghun.where.where.Fragment;
 
 import android.Manifest;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.BinderThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.parkjunghun.where.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -23,9 +23,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-import java.util.concurrent.Executor;
-
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
@@ -35,17 +34,35 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    protected Location mLastLocation;
+    private GoogleMap gMap;
+    protected Location lastLocation;
     private com.google.android.gms.maps.MapFragment mapFragment;
 
-    private FusedLocationProviderClient mFusedLocationClient;
+    private FusedLocationProviderClient gFusedLocationClient;
     private static final int RC_LOCATION = 1;
+
+    private Button currentLocation;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.map_fragment,container,false);
+        View view = inflater.inflate(R.layout.map_fragment, null);
+
+
+        currentLocation = (Button) view.findViewById(R.id.currentlocation);
+        currentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gMap.clear();
+
+                LatLng location = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+
+                gMap.addMarker(new MarkerOptions().position(location).title("현재위치"));
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,14));
+                gMap.animateCamera(CameraUpdateFactory.zoomTo(14),2000,null);
+            }
+        });
+
         return view;
     }
 
@@ -54,8 +71,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         super.onViewCreated(view, savedInstance);
 
         mapFragment = (com.google.android.gms.maps.MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        gFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         mapFragment.getMapAsync(this);
+
+
+
     }
 
 
@@ -63,20 +83,20 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @AfterPermissionGranted(RC_LOCATION) // automatically re-invoke this method after getting the required permission
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        gMap = googleMap;
 
         String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
         if (EasyPermissions.hasPermissions(getActivity(), perms)) {
-            mFusedLocationClient.getLastLocation().addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+            gFusedLocationClient.getLastLocation().addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
                 @Override
                 public void onComplete(@NonNull Task<Location> task) {
                     if (task.isSuccessful() && task.getResult() != null) {
-                        mLastLocation = task.getResult();
+                        lastLocation = task.getResult();
 
-                        LatLng location = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-                        mMap.addMarker(new MarkerOptions().position(location).title("현재위치"));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
-                        mMap.animateCamera(CameraUpdateFactory.zoomTo(17),2000,null);
+                        LatLng location = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                        gMap.addMarker(new MarkerOptions().position(location).title("현재위치"));
+                        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,14));
+                        gMap.animateCamera(CameraUpdateFactory.zoomTo(14),2000,null);
                     }
                 }
             });
