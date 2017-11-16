bd151a7925a9a3dd6eb35060608f8da7fe93804c
tmdgks <tmdgks1006>
2017-11-16 21:09:22 +0900

    kjkjkiok

---
+++ app/src/main/java/com/example/parkjunghun/where/where/Activity/Login2Activity.java
@@ -0,0 +1,106 @@
+package com.example.parkjunghun.where.where.Activity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.parkjunghun.where.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonSignin;
+    TextView textviewSingin;
+    TextView textviewMessage;
+    TextView textviewFindPassword;
+    ProgressDialog progressDialog;
+    //define firebase object
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        //initializig firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        //initializing views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textviewSingin = (TextView) findViewById(R.id.textViewSignin);
+        textviewMessage = (TextView) findViewById(R.id.textviewMessage);
+        textviewFindPassword = (TextView) findViewById(R.id.textViewFindpassword);
+        buttonSignin = (Button) findViewById(R.id.buttonSignup);
+        progressDialog = new ProgressDialog(this);
+
+        //button click event
+        buttonSignin.setOnClickListener(this);
+        textviewSingin.setOnClickListener(this);
+        textviewFindPassword.setOnClickListener(this);
+    }
+
+    //firebase userLogin method
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "email을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "password를 입력해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("로그인중입니다. 잠시 기다려 주세요...");
+        progressDialog.show();
+
+        //logging in the user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "로그인 실패!", Toast.LENGTH_LONG).show();
+                            textviewMessage.setText("로그인 실패 유형\n - password가 맞지 않습니다.\n -서버에러");
+                        }
+                    }
+                });
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSignin) {
+            userLogin();
+        }
+        if (view == textviewSingin) {
+            finish();
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
+        if (view == textviewFindPassword) {
+            finish();
+            startActivity(new Intent(this, FindActivity.class));
+        }
+    }
+}
