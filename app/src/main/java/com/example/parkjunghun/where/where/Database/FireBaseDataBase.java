9a10cafdd09a6d9d1bfb2b1a10fa7e8d429547d5
lenovo <hjun4512@naver.com>
2017-11-17 11:38:37 +0900

    FireBase DataBase

---
+++ app/src/main/java/com/example/parkjunghun/where/where/Database/FireBaseDataBase.java
@@ -0,0 +1,20 @@
+package com.example.parkjunghun.where.where.Database;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by lenovo on 2017-11-17.
+ */
+
+public class FireBaseDataBase {
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    public FireBaseDataBase(){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+
+}
