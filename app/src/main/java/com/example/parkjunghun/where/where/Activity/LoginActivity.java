16acee63686a87eba0e02a2e058bfaf1c2ddd55c
parkjunghun <go9018@naver.com>
2017-12-09 13:33:50 +0900

    디자인수정/아이디비번저장(아이디비번치고 체크박스체크)

--- app/src/main/java/com/example/parkjunghun/where/where/Activity/LoginActivity.java
+++ app/src/main/java/com/example/parkjunghun/where/where/Activity/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.parkjunghun.where.where.Activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,6 +33,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     //define firebase object
     private FirebaseAuth firebaseAuth;
     String phonenum;
+    CheckBox checkBox;
+    SharedPreferences autologin;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +51,40 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         textviewFindPassword = (TextView) findViewById(R.id.textViewFindpassword);
         buttonSignin = (Button) findViewById(R.id.buttonSignup);
         progressDialog = new ProgressDialog(this);
+        checkBox = (CheckBox)findViewById(R.id.autologin_checkbox);
 
         buttonSignin.setOnClickListener(this);
         textviewSingin.setOnClickListener(this);
         textviewFindPassword.setOnClickListener(this);
+
+
+        autologin = getSharedPreferences("setting", 0);
+        editor= autologin.edit();
+
+        if(autologin.getBoolean("autologin", false)){
+            editTextEmail.setText(autologin.getString("ID", ""));
+            editTextPassword.setText(autologin.getString("PW", ""));
+            checkBox.setChecked(true);
+        }
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if(isChecked){
+                    String ID = editTextEmail.getText().toString();
+                    String PW = editTextPassword.getText().toString();
+
+                    editor.putString("ID", ID);
+                    editor.putString("PW", PW);
+                    editor.putBoolean("autologin", true);
+                    editor.commit();
+                }else{
+                    editor.clear();
+                    editor.commit();
+                }
+            }
+        });
     }
 
     private void userLogin() {
